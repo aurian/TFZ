053373e52024d13a7ed8d214f09c8745ed0fecf0
Aurian de Maupeou <aurian.demaupeou@gmail.com>
2011-10-26 10:23:58 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterForZombies::Application.config.secret_token = '126a30fb201be2bad6db2ea18647d902f5b25b03e1b0b48bad2c427783aea43b726644a15063c1b408e29b8c4566774c37aad3d7c3e5957ecda86841499c0011'
---
+++ db/migrate/20111025183144_create_zombies.rb
@@ -0,0 +1,8 @@
+class CreateZombies < ActiveRecord::Migration
+  def change
+    create_table :zombies do |t|
+
+      t.timestamps
+    end
+  end
+end
